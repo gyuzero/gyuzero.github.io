1b1219c02243e59c2357c4f52f2562d6aae22c19
gyuzero <gyuzero0103@gmail.com>
2025-02-07 10:46:01 +0900

    initial commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  title: `gyuzero.github.io`,
+  description: `규제로의 개발일기`,
+  language: `ko`, // `ko`, `en` => currently support versions for Korean and English
+  siteUrl: `https://gyuzero.github.io`,
+  ogImage: `/og-image.png`, // Path to your in the 'static' folder
+  comments: {
+    utterances: {
+      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+    },
+  },
+  ga: '0', // Google Analytics Tracking ID
+  author: {
+    name: `박규영`,
+    bio: {
+      role: `개발자`,
+      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+    },
+    social: {
+      github: ``, // `https://github.com/zoomKoding`,
+      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: ``, // `zoomkoding@gmail.com`,
+    },
+  },
+
+  // metadata for About Page
+  about: {
+    timestamps: [
+      // =====       [Timestamp Sample and Structure]      =====
+      // ===== 🚫 Don't erase this sample (여기 지우지 마세요!) =====
+      {
+        date: '',
+        activity: '',
+        links: {
+          github: '',
+          post: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+    ],
+
+    projects: [
+      // =====        [Project Sample and Structure]        =====
+      // ===== 🚫 Don't erase this sample (여기 지우지 마세요!)  =====
+      {
+        title: '',
+        description: '',
+        techStack: ['', ''],
+        thumbnailUrl: '',
+        links: {
+          post: '',
+          github: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+    ],
+  },
+};
